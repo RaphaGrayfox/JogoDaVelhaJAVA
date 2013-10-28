fad112ae17e5e62be271e4420af252ae2b945f6c
GrayFox <raphagrayfox@gmail.com>
2013-10-28 20:48:17 -0200

    Meu primeiro jogo em Java ... Jogo da velha

---
+++ src/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,11 @@
+
+package jogodavelha;
+
+import view.Jogo;
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+        jogo.setVisible(true);
+    }
+}
